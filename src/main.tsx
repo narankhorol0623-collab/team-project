355a11b5fb7b66a87e6cc8ca680ddfbfb9c054ef
Mend-amr <mmendamar7@gmail.com>
2026-04-01 11:28:42 +0800

    mende

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import Page from "./app/page";
 
 createRoot(document.getElementById("root")!).render(
