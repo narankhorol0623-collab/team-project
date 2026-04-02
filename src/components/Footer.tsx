c5d4242418617c55457facea04a60296355be021
tmka5301-rgb <tmka5301@gmail.com>
2026-04-02 12:01:44 +0800

    barag duussan

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,7 +22,6 @@ export default function Footer() {
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
             &copy; 2026 ЗөвБич.
-            {/* Бүх эрх хуулиар хамгаалагдсан. */}
           </p>
         </div>
       </div>
